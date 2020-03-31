ca1c8921149c0f7acd80d9ac7286b0963de5b44e
tsy19025 <45425656+tsy19025@users.noreply.github.com>
2020-04-01 01:14:36 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,23 +6,37 @@ from torch.utils.data import Dataset
 class IMDBdataset(Dataset):
     def __init__(self, data_path, word2vec):
         self.review = []
-        maindir, subdir, file_name_list = os.walk(data_path + 'pos/')
+        file_name_list = os.listdir(data_path + 'pos/')
+        tot = 0
+        self.mv = 0
         for filename in file_name_list:
-            with open(filename, 'r') as f:
-                self.review.append([f.read(), 1])
+            tot += 1
+            if tot >= 100: break
+            with open(data_path + 'pos/' + filename, 'r') as f:
+                s = f.read().split(' ')
+                self.mv = max(self.mv, len(s))
+                self.review.append([s, 1])
             
-        main_dir, sub_dir, file_name_list = os.walk(data_path + 'neg/')
+        file_name_list = os.listdir(data_path + 'neg/')
         for filename in file_name_list:
-            with open(filename, 'r') as f:
-                self.review.append([f.read(), 0])
+            tot += 1
+            if tot >= 200: break
+            with open(data_path + 'neg/' + filename, 'r') as f:
+                s = f.read().split(' ')
+                self.mv = max(self.mv, len(s))
+                self.review.append([s, 0])
         random.shuffle(self.review)
         self.word2vec = word2vec
     
     def sentence2vec(self, sentence):
         input = []
         for word in sentence:
-            input.append(self.word2vec[word])
-        return torch.tensor(input)
+            try: input.append(self.word2vec[word])
+            except: continue
+        data = torch.tensor(input)
+        tmp = torch.zeros(self.mv - data.shape[0], data.shape[1])
+        data = torch.cat([data, tmp], 0)
+        return data
     
     def __getitem__(self, index):
         return self.sentence2vec(self.review[index][0]), self.review[index][1]
