bec495735898e4162da3b978f536dcf14bbbb68d
tsy19025 <45425656+tsy19025@users.noreply.github.com>
2020-04-01 01:07:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import torch
-import torch.nn as .nn
+import torch.nn as nn
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
+from utils import IMDBdataset
+from model import Model
 from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
 import sys
 import os
 import argparse
@@ -10,37 +15,42 @@ def parse_args():
     parse.add_argument('--epochs', type = int, default = 100)
     parse.add_argument('--batch_size', type = int, default = 64)
     parse.add_argument('--word2vec_save', type = str, default = "word2vec")
-    parse.add_argument('--word2vec', type = bool, default = True)
+    parse.add_argument('--word2vec', type = bool, default = False)
     parse.add_argument('--save', type = str, default = "model/model.pth")
+    parse.add_argument('--emb_d', type = int, default = 512)
+    parse.add_argument('--hidden_d', type = int, default = 128)
+    parse.add_argument('--nlayer', type = int, default = 2)
+    parse.add_argument('--lr', type = float, default = 0.00005)
     return parse.parse_args()
 
 def get_word2vec(args):
     sentences = ""
-    for data_path in ['train', 'valid', 'test']:
-        maindir, subdir, file_name_list = os.walk(data_path + 'pos/')
-        for filename in file_name_list:
-            with open(filename, 'r') as f:
-                sentences = sentences + f.read() + '\n'
-        maindir, subdir, file_name_list = os.walk(data_path + 'neg/')
-        for filename in file_name_list:
-            with open(filename, 'r') as f:
-                sentences = sentences + f.read() + '\n'
-    with open('./all_sentences.txt', 'w') as f2:
-        f2.write(sentences)
+    if os.path.exists('./all_sentences.txt') == False:
+        for data_path in ['train/', 'valid/', 'test/']:
+            fpath = data_path + 'pos/'
+            file_name_list = os.listdir(fpath)
+            for filename in file_name_list:
+                with open(fpath + filename, 'r') as f:
+                    sentences = sentences + f.read() + '\n'
+            fpath = data_path + 'neg/'
+            file_name_list = os.listdir(fpath)
+            for filename in file_name_list:
+                with open(fpath + filename, 'r') as f:
+                    sentences = sentences + f.read() + '\n'
+        with open('./all_sentences.txt', 'w') as f2:
+            f2.write(sentences)
     
-    sentences = word2vec.LineSentence('all_sentences.txt')
-    word2vec = Word2Vec(sentences, hs = 1, min_count = 5, window = 5,size = 512)
+    sentences = LineSentence('all_sentences.txt')
+    word2vec = Word2Vec(sentences, hs = 1, min_count = 5, window = 5,size = args.emb_d)
     word2vec.save(args.word2vec_save)
 
-def train_one_epoch(model, train_data_loader, optimizer, loss_fn, device):
+def train_one_epoch(model, train_data_loader, optimizer, loss_fn):
     train_loss = []
     for step, data in enumerate(train_data_loader):
         input, label = data
-        input = input.to(device)
-        label = label.to(device)
-        
         output = model(input)
         loss = loss_fn(output, label)
+        train_loss.append(loss)
 
 def same(a, b):
     tot = 0
@@ -54,20 +64,17 @@ def valid(model, valid_data_loader):
     tot = 0
     for step, data in enumerate(train_data_loader):
         input, label = data
-        input = input.to(device)
-        label = label.to(device)
-        
         output = model(input)
         tot += len(output)
         current += same(label, output)
     return 1.0 * current / tot
   
-if "__name__" == __main__:
+if __name__ == "__main__":
     torch.multiprocessing.set_sharing_strategy('file_system')
     args = parse_args()
     
     if args.word2vec == True:
-        get_word2vec(args, args.data_path)
+        get_word2vec(args)
     
     word2vec = Word2Vec.load(args.word2vec_save)  
     train_data_loader = DataLoader(dataset = IMDBdataset('train/', word2vec),
@@ -75,30 +82,25 @@ if "__name__" == __main__:
                                        shuffle = True,
                                        num_workers = 20, pin_memory = True)
     valid_data_loader = DataLoader(dataset = IMDBdataset('valid/', word2vec),
-                                       batch_size = args.batch_size,
+                                       batch_size = 1, # args.batch_size,
                                        shuffle = True,
-                                       num_workers = 20, pin_memory = True))
+                                       num_workers = 20, pin_memory = True)
     test_data_loader = DataLoader(dataset = IMDBdataset('test/', word2vec),
-                                       batch_size = args.batch_size,
+                                       batch_size = 1, # args.batch_size,
                                        shuffle = True,
-                                       num_workers = 20, pin_memory = True))
+                                       num_workers = 20, pin_memory = True)
     
-    use_cuda = torch.cuda.is_available() and args.cuda
-    device = torch.device('cuda' if use_cuda else 'cpu')
-    model = Model()
-    model = model.to(device)
-    
-    loss_fn = nn.BCELoss(reduction = 'none').to(device)
+    model = Model(args.emb_d, args.hidden_d, args.nlayer)
+    loss_fn = nn.BCELoss(reduction = 'none')
     optimizer = torch.optim.Adam(model.parameters(), lr = args.lr,
                                  weight_decay = 0.000001)
     scheduler = lr_scheduler.StepLR(optimizer, step_size = 30, gamma = 0.1)
     
     best_acc = 0
     best_epoch = -1
-    for epoch in args.epochs:
-        print("Epoch", epoch);
-        mean_loss = train_one_epoch(model, train_data_loader, optimizer, 
-                                    loss_fn, epoch)
+    for epoch in range(args.epochs):
+        print("Epoch", epoch)
+        mean_loss = train_one_epoch(model, train_data_loader, optimizer, loss_fn)
         acc = valid(model, valid_data_loader)
         print("Valid: Acc", acc)
         if acc > best_acc:
@@ -113,6 +115,6 @@ if "__name__" == __main__:
     # Test
     state = torch.load(args.save)
     model.load_state_dict(state['net'])
-    model.to(device)
+    # model.to(device)
     acc = valid(test_data_load, test_data_loader)
     print("Test: Acc", acc)
